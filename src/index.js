55416647bdf8d41c25353397201eba3feb97db8c
woink <wardprice@gmail.com>
2020-08-25 15:30:04 -0400

    required deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskItem = document.getElementById('new-task-description')
+  const newTaskForm = document.getElementById('create-task-form')
+  const taskContainer = document.getElementById('tasks')
+
+  let newTasks = []
+
+  newTaskForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+    newTasks.push(
+      `<li id="${newTaskItem.value}" > 
+      ${newTaskItem.value} <button data-id="${newTaskItem.value}" class="delete-comment" >x</button> </li>`)
+  	taskContainer.innerHTML = newTasks.join("")
+  })
+
+  taskContainer.addEventListener("click", function(e) {
+    if (e.target.className == "delete-comment") {
+      let taskId = e.target.dataset.id
+      let deleteTask = document.getElementById(taskId)
+      let deleteIndex = newTasks.indexOf(deleteTask)
+      newTasks.splice(deleteIndex, 1)
+      deleteTask.remove()
+    }
+  })
 });
